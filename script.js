1ac83b8f57a07fd26a5336c0faf34009c1893dbd
jarvis1234d <indtarun5@gmail.com>
2020-05-27 15:38:12 +0530

    Got the display working properly

--- script.js
+++ script.js
@@ -7,9 +7,23 @@ $(document).ready(function () {
 
         $(e.currentTarget).addClass('shadow');
         // $('div').addClass('shadow');
-    })
+    });
     $('button').on("click", function (e) {
+        var pre = $('.output').text()
         var value = $(e.currentTarget).text()
-        console.log(value);
-    })
+
+        if (value == "AC") {
+            $('.output').text(0);
+        } else {
+            if (pre == 0) {
+                $('.output').text(value);
+            } else {
+                $('.output').text(pre + value);
+            };
+
+        };
+
+
+
+    });
 });
